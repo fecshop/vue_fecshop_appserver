9db0398f10adff34f2a54e896d6d771f209a996f
Terry <2358269014@qq.com>
2018-02-26 09:31:34 +0800

    勾选购物车：优化代码实现

--- src/views/body/checkout/Cart.vue
+++ src/views/body/checkout/Cart.vue
@@ -25,13 +25,13 @@
                     
                     <div class="cart_info">
                         <div class="cart_select_div">
-                            <input @click="selectAll(selectAllStatus)"  v-model="selectAllStatus" id="cart_select_all" type="checkbox" name="cart_select_all" class="cart_select cart_select_all">
+                            <input @click="selectAll()"  v-model="selectAllStatus" id="cart_select_all" type="checkbox" name="cart_select_all" class="cart_select cart_select_all">
                             &nbsp;
                             <label for="cart_select_all">Select All Product</label>
                         </div>
                         <div v-for="(productOne,index) in cart_products " class="row">
                             <div class="col-33">
-                                <input @click="selectOne(productOne.item_id, productOne.active)" v-model="productOne.active" type="checkbox" name="cart_select_item" class="cart_select cart_select_item">
+                                <input @click="selectOne(productOne)" v-model="productOne.active" type="checkbox" name="cart_select_item" class="cart_select cart_select_item">
                                 <router-link :to="productOne.url" title="productOne.name"  class="product-image">
                                     <img :src="productOne.img_url"   alt="productOne.name" width="75" height="75">
                                 </router-link>
@@ -303,7 +303,11 @@ export default {
                 }
             });
         },
-        selectOne: function(item_id, active){
+        selectOne: function(productOne){
+            productOne.active = !productOne.active;
+            var item_id = productOne.item_id;
+            var active = productOne.active;
+            
             var self = this;
             self.errormsg = '';
             $.showIndicator();
@@ -338,11 +342,16 @@ export default {
             });
         },
         
-        selectAll: function(active){
+        selectAll: function(){
             var self = this;
             self.errormsg = '';
             $.showIndicator();
-            active = active ? 1 : 0;
+            if (!self.selectAllStatus) {
+                self.selectAllStatus = 1;
+            } else {
+                self.selectAllStatus = 0;
+            }
+            var active = self.selectAllStatus;
             var url = self.selectAllUrl;
             $.ajax({
                 url: url,
